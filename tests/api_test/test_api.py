e89af02301530d101a961dee531fe2200005d532
pkpoornima27 <pk.poornima@gmail.com>
2026-02-13 11:07:34 +0000

    Updating the api tests

--- tests/api_test/test_api.py
+++ tests/api_test/test_api.py
@@ -1,6 +1,6 @@
 import requests
 
-base_url = "https://dotesthere.com/api/"
+base_url = "https://dotesthere.com/api"
 
 headers = {
          "Content-Type": "application/json"
@@ -28,6 +28,7 @@ def test_get_api_one_records():
     assert data['first_name'] == 'Janet'
     assert data['last_name'] == 'Weaver'
 
+
 def test_create_update_delete_api():
     payload = {
             "email": "pk.poornima@gmail.com",
@@ -35,48 +36,11 @@ def test_create_update_delete_api():
             "last_name": "Kemparaju",
             "job": "Test Automation Engineer"
         }
-    response = requests.post()
+    response_post = requests.post(f"{base_url}/users", json=payload, headers=headers)
 
-# def test_post_api():
-#     url = "https://dotesthere.com/api/users"
-#     payload = {
-#         "email": "pk.poornima@gmail.com",
-#         "first_name": "Poornima",
-#         "last_name": "Kemparaju",
-#         "job": "Test Automation Engineer"
-#     }
-#     headers = {
-#         "Content-Type": "application/json"
-#     }
-#     response = requests.post(url, json=payload, headers=headers)
-#     assert response.status_code == 201
-#     assert (response.json()['data']['id']) == 3
-#     assert (response.json()['data']['email']) == "pk.poornima@gmail.com"
-#     assert (response.json()['data']['first_name']) == "Poornima"
-#     assert (response.json()['data']['last_name']) == "Kemparaju"
-#     assert (response.json()['data']['job']) == "Test Automation Engineer"
-#
-# def test_put_api():
-#     url = "https://dotesthere.com/api/users/1"
-#     payload = {
-#         "email": "natarajmb@gmail.com",
-#         "first_name": "Nataraj",
-#         "last_name": "Basappa",
-#         "job": "CTO"
-#     }
-#     headers = {
-#         "Content-Type": "application/json"
-#     }
-#     response = requests.put(url, json=payload, headers=headers)
-#     assert response.status_code == 200
-#     assert (response.json()['data']['id']) == 1
-#     assert (response.json()['data']['email']) == "natarajmb@gmail.com"
-#     assert (response.json()['data']['first_name']) == "Nataraj"
-#     assert (response.json()['data']['last_name']) == "Basappa"
-#     assert (response.json()['data']['avatar']) == "https://dotesthere.com/img/faces/1-image.jpg"
-#     assert (response.json()['data']['job']) == "CTO"
-#
-# def test_delete_api():
-#     url = "https://dotesthere.com/api/users/1"
-#     response = requests.delete(url)
-#     assert  response.status_code == 204
+    assert response_post.status_code == 201
+    user =  response_post.json()["data"]
+    assert user["email"] == "pk.poornima@gmail.com"
+
+
+
